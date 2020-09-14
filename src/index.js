071974905fdf4b7aaffca1a36394a6375de142c7
Ryō Igarashi <n33t5hin@gmail.com>
2020-09-15 04:14:42 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+
+ReactDOM.render(
+  <div className="App">
+    <h1>antd version: {version}</h1>
+    <DatePicker />
+    <Button type="primary" style={{ marginLeft: 8 }}>
+      Primary Button
+    </Button>
+  </div>,
+  document.getElementById("root")
+);
