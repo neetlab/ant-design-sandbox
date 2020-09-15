0d43905477aaaaa23b3be41a25f93795d5871694
Ryo Igarashi <n33t5hin@gmail.com>
2020-09-15 15:26:42 +0900

    aa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button, DatePicker, version } from "antd";
+import { Button, DatePicker, Typography, Row, Col, Space } from "antd";
 import "antd/dist/antd.css";
 import "./index.css";
 
+const Vertical = () => {
+  return (
+    <React.Fragment>
+      <Typography.Title level={2}>
+        Vertical grid
+      </Typography.Title>
+
+      <Col>
+        <Row gutter={[0, 12]}>
+          <Button>foo</Button>
+        </Row> 
+
+        <Row>
+          <Button>foo</Button>
+        </Row> 
+      </Col>
+    </React.Fragment>
+  )
+}
+
+const Horizontal = () => {
+  return (
+    <React.Fragment>
+      <Typography.Title level={2}>Grid</Typography.Title>
+      <Row justify="space-around">
+        <Col>
+          foo
+        </Col>
+
+        <Col>
+          bar
+        </Col>
+      </Row>
+
+      <Typography.Title level={2}>Spacing</Typography.Title>
+      <Typography.Paragraph>
+        sizeとかalignとかいろいろできるけど、justifyは無いみたい
+      </Typography.Paragraph>
+
+      <Space size='large'>
+        <Typography.Paragraph>foo</Typography.Paragraph>
+        <Typography.Paragraph>bar</Typography.Paragraph>
+      </Space>
+    </React.Fragment>
+  );
+}
+
 ReactDOM.render(
   <div className="App">
-    <h1>antd version: {version}</h1>
-    <DatePicker />
-    <Button type="primary" style={{ marginLeft: 8 }}>
-      Primary Button
-    </Button>
+    <Vertical />
+    <Horizontal />
   </div>,
   document.getElementById("root")
 );
